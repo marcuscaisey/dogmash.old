efd55946cac7b9f80c9a1cbb07e5f27839243116
Marcus Caisey <marcus@teckna.com>
2018-12-16 21:47:59 +0000

    Add Dog peewee model

---
+++ dogmash/models.py
@@ -0,0 +1,20 @@
+import pathlib
+
+import peewee
+
+current_dir = pathlib.Path(__file__).parent
+PRAGMAS = {"journal_mode": "wal"}
+db = peewee.SqliteDatabase(current_dir / "db.sqlite", pragmas=PRAGMAS)
+
+
+class Dog(peewee.Model):
+    """Dog model which holds a dog's rating and image file name."""
+
+    file_name = peewee.TextField(unique=True)
+    rating = peewee.IntegerField()
+
+    class Meta:
+        database = db
+
+
+Dog.create_table()
