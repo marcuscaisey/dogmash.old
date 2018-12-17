e00bc4eccb36408923532206b499d3da20319252
Marcus Caisey <marcus@teckna.com>
2018-12-17 23:41:19 +0000

    Move database fixtures to conftest.py so that they're available in other modules

---
+++ tests/integration/conftest.py
@@ -0,0 +1,53 @@
+import random
+import string
+
+import peewee
+import pytest
+
+from dogmash import config, models
+
+
+@pytest.fixture(scope="module")
+def db():
+    """Empty test database to be used in all tests."""
+    db = peewee.SqliteDatabase(":memory:")
+    models.Dog.bind(db)
+    models.Dog.create_table()
+    return db
+
+
+@pytest.fixture()
+def txn(db):
+    """Transaction fixture which automatically rolls back all changes after
+    each test has finished, regardless of the result."""
+    with db.atomic() as txn:
+        yield
+        txn.rollback()
+
+
+@pytest.fixture
+def create_dog(create_random_file_name):
+    """Factory which returns a dog with a given file name and a rating of
+    `config.DOG_INITIAL_RATING`. If no file name is given, then the dog is
+    created with a random file name."""
+
+    def dog_factory(file_name=None):
+        file_name = (
+            create_random_file_name() if file_name is None else file_name
+        )
+        rating = config.DOG_INITIAL_RATING
+        return models.Dog.create(file_name=file_name, rating=rating)
+
+    return dog_factory
+
+
+@pytest.fixture
+def create_random_file_name():
+    """Factory which returns a random image file name."""
+
+    def random_file_name_factory():
+        length = random.randint(10, 15)
+        chars = string.ascii_letters + string.digits + "-_"
+        return f"{''.join(random.choice(chars) for _ in range(length))}.jpg"
+
+    return random_file_name_factory
--- tests/integration/test_operations_integ.py
+++ tests/integration/test_operations_integ.py
@@ -1,60 +1,10 @@
-import random
-import string
-
-import peewee
 import pytest
 
 from dogmash import config, exceptions, models, operations
 
 
-@pytest.fixture(scope="module")
-def db():
-    """Empty test database to be used in all tests."""
-    db = peewee.SqliteDatabase(":memory:")
-    models.Dog.bind(db)
-    models.Dog.create_table()
-    return db
-
-
-@pytest.fixture(autouse=True)
-def txn(db):
-    """Transaction fixture which automatically rolls back all changes after
-    each test has finished, regardless of the result."""
-    with db.atomic() as txn:
-        yield
-        txn.rollback()
-
-
-@pytest.fixture
-def create_dog(create_random_file_name):
-    """Factory which returns a dog with a given file name and a rating of
-    `config.DOG_INITIAL_RATING`. If no file name is given, then the dog is
-    created with a random file name."""
-
-    def dog_factory(file_name=None):
-        file_name = (
-            create_random_file_name() if file_name is None else file_name
-        )
-        rating = config.DOG_INITIAL_RATING
-        return models.Dog.create(file_name=file_name, rating=rating)
-
-    return dog_factory
-
-
-@pytest.fixture
-def create_random_file_name():
-    """Factory which returns a random image file name."""
-
-    def random_file_name_factory():
-        length = random.randint(10, 15)
-        chars = string.ascii_letters + string.digits + "-_"
-        return f"{''.join(random.choice(chars) for _ in range(length))}.jpg"
-
-    return random_file_name_factory
-
-
 @pytest.mark.parametrize("dog_exists", [True, False])
-def test_add_dog(monkeypatch, create_dog, dog_exists):
+def test_add_dog(txn, monkeypatch, create_dog, dog_exists):
     monkeypatch.setattr(config, "DOG_INITIAL_RATING", 2000)
 
     if dog_exists:
@@ -76,14 +26,19 @@ def test_create_dog_table(db):
     assert models.Dog.table_exists()
 
 
-def test_dogs(create_dog):
+def test_dogs(txn, create_dog):
     dogs = [create_dog() for _ in range(50)]
     assert operations.dogs() == dogs
 
 
 @pytest.mark.parametrize("dog_in_table", [True, False])
 def test_fill_database(
-    monkeypatch, tmp_path, create_random_file_name, create_dog, dog_in_table
+    txn,
+    monkeypatch,
+    tmp_path,
+    create_random_file_name,
+    create_dog,
+    dog_in_table,
 ):
     monkeypatch.setattr(config, "DOG_IMAGES_DIR", tmp_path)
 
