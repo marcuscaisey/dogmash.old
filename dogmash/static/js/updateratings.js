99a1ba07894a0362c473fde6fbb3cade1a2fc5de
Marcus Caisey <marcus@teckna.com>
2018-12-27 00:16:20 +0000

    Refactor updateratings.js

--- dogmash/static/js/updateratings.js
+++ dogmash/static/js/updateratings.js
@@ -1,31 +1,36 @@
-const fadeDuration = 300;
+const fadeDuration = 250;
 const transitionDuration = 500;
 
 $(document).ready(function () {
     $("img.dog").click(function () {
-        let winnerId = parseInt($(this).attr("id"));
+        let winnerId = $(this).attr("id");
         let loserId;
-
-        for (let img of $("img.dog")) {
-            let id = parseInt(img.id);
-            if (id != winnerId) {
-                loserId = id;
-            }
+        let winnerDog;
+        let loserDog;
+        let dogs = $("img.dog");
+        if (dogs[0].id === winnerId) {
+            loserId = dogs[1].id;
+            winnerDog = 1;
+            loserDog = 2;
+        } else {
+            loserId = dogs[0].id;
+            winnerDog = 2;
+            loserDog = 1;
         }
 
-        $.post("updateratings", {
+        let request = {
             "winner_id": winnerId,
             "loser_id": loserId,
-        }, function (data) {
-            $(`#rank${winnerId}`).text(data["winner"]["rank"]);
-            $(`#rating${winnerId}`).text(data["winner"]["rating"]);
-            $(`#rank${loserId}`).text(data["loser"]["rank"]);
-            $(`#rating${loserId}`).text(data["loser"]["rating"]);
-
-            $(`#rank-change${winnerId}`).text(`(${data["winner"]["rank_change"]})`).addClass("winner");
-            $(`#rating-change${winnerId}`).text(`(${data["winner"]["rating_change"]})`).addClass("winner");
-            $(`#rank-change${loserId}`).text(`(${data["loser"]["rank_change"]})`).addClass("loser");
-            $(`#rating-change${loserId}`).text(`(${data["loser"]["rating_change"]})`).addClass("loser");
+        }
+        $.post("updateratings", request, function (response) {
+            $(`#rank${winnerDog}`).text(response["winner"]["rank"]);
+            $(`#rating${winnerDog}`).text(response["winner"]["rating"]);
+            $(`#rank${loserDog}`).text(response["loser"]["rank"]);
+            $(`#rating${loserDog}`).text(response["loser"]["rating"]);
+            $(`#rank-change${winnerDog}`).text(`(${response["winner"]["rank_change"]})`).addClass("winner");
+            $(`#rating-change${winnerDog}`).text(`(${response["winner"]["rating_change"]})`).addClass("winner");
+            $(`#rank-change${loserDog}`).text(`(${response["loser"]["rank_change"]})`).addClass("loser");
+            $(`#rating-change${loserDog}`).text(`(${response["loser"]["rating_change"]})`).addClass("loser");
 
             // Fix position of ranks/ratings so they don't get re-centred.
             $(".rank-container, .rating-container").each(function () {
@@ -47,30 +52,32 @@ $(document).ready(function () {
 
             $(".change").fadeIn(fadeDuration).promise().done(function () {
                 setTimeout(function () {
-                    // Need to wait for images to load until fading all
-                    // elements in, so that they fade in at the same time.
-                    $("img.dog").on("load", function () {
-                        $(this).fadeIn(fadeDuration);
-                        $(".rank, .rating").fadeIn(fadeDuration);
-                        $("*").removeAttr("style");
-                    });
-
-                    $("img.dog, .rank, .rating, .change").fadeOut(fadeDuration).promise().done(function () {
-                        $(`#rank-change${winnerId}`).attr("id", `rank-change${data["dog1"]["id"]}`);
-                        $(`#rank-change${loserId}`).attr("id", `rank-change${data["dog2"]["id"]}`);
-                        $(`#rating-change${winnerId}`).attr("id", `rating-change${data["dog1"]["id"]}`);
-                        $(`#rating-change${loserId}`).attr("id", `rating-change${data["dog2"]["id"]}`);
-                        $(".change").removeClass("winner").removeClass("loser");
-
-                        $(`#rank${winnerId}`).text(data["dog1"]["rank"]).attr("id", `rank${data["dog1"]["id"]}`);
-                        $(`#rank${loserId}`).text(data["dog2"]["rank"]).attr("id", `rank${data["dog2"]["id"]}`);
-                        $(`#rating${winnerId}`).text(data["dog1"]["rating"]).attr("id", `rating${data["dog1"]["id"]}`);
-                        $(`#rating${loserId}`).text(data["dog2"]["rating"]).attr("id", `rating${data["dog2"]["id"]}`);
-                        $(`#${winnerId}`).attr("src", `images/${data["dog1"]["file_name"]}`).attr("id", data["dog1"]["id"]);
-                        $(`#${loserId}`).attr("src", `images/${data["dog2"]["file_name"]}`).attr("id", data["dog2"]["id"]);
+                    $(".rating, .rank, .change, .dog").fadeOut(fadeDuration).promise().done(function () {
+                        $(".change").removeClass("winner loser");
+                        $(`#rank${winnerDog}`).text(response["dog1"]["rank"]);
+                        $(`#rank${loserDog}`).text(response["dog2"]["rank"]);
+                        $(`#rating${winnerDog}`).text(response["dog1"]["rating"]);
+                        $(`#rating${loserId}`).text(response["dog2"]["rating"]);
+                        $(`#${winnerId}`).attr("src", `images/${response["dog1"]["file_name"]}`).attr("id", response["dog1"]["id"]);
+                        $(`#${loserId}`).attr("src", `images/${response["dog2"]["file_name"]}`).attr("id", response["dog2"]["id"]);
                     });
                 }, transitionDuration)
             });
         });
+    });
+
+    // Need to wait for all images to load until fading all elements in, so
+    // that they fade in at the same time.
+    let imagesLoaded = 0;
+    $("img.dog").on("load", function () {
+        imagesLoaded++;
+        if (imagesLoaded === 2) {
+            $(".dog, .rank, .rating").fadeIn(fadeDuration);
+            // Remove all custom styles once elements have been faded back in,
+            // so that all elements can be automatically re-aligned and resized
+            // again.
+            $("*").removeAttr("style");
+            imagesLoaded = 0;
+        }
     });
 });
